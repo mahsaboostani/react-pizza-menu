177a5ba5270e53ff478910472aa9f1251e96147e
mahsaboostani <eng.m.boostani@gmail.com>
2023-09-13 21:49:15 +0200

    add css and jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const pizzaData = [
+    {
+        name: "Foccacia",
+        ingredients: "Bread with italian olive oil",
+        price: 6,
+        photoName: "pizzas/image.jpeg",
+        soldOut: false
+    }
+]
+function App() {
+    return <div className="container"><Header /><Menu /><Footer />
+    </div>
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Header() {
+    const style = { color: "red", fontSize: "48px", textTransform: "uppercase" }
+    return <h1 className="headers">Fast react pizaa company</h1>
+}
+
+function Menu() {
+    return <main className="menu"><h2>Our menu</h2><Pizza /></main >
+}
+function Footer() {
+    const hour = new Date().getHours()
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour
+    // if (hour >= openHour && hour <= closeHour) alert("er are currently open"); else alert("sorry we are closed")
+    return <footer className="footer">{new Date().toLocaleTimeString()}. We are currently open</footer>
+    // return React.createElement('footer', null, 'Were currently open')
+
+}
+
+function Pizza() {
+    return <div className="pizza">
+        <img src="pizzas/image.jpeg" alt="Pizza spinachi" />
+        <h2>Pizza spinachi</h2>
+        <p>tomato, olive oil</p>
+    </div>
+
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<React.StrictMode><App /></React.StrictMode>)
